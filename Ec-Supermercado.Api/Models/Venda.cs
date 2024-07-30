e9e5a0a4a7dba408ebc839578df610ce95cdb025
HugoNovais <hugo.o.novais@outlook.com>
2024-07-30 16:27:15 -0300

    Inicio da implementação de Venda

---
+++ Ec-Supermercado.Api/Models/Venda.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ec_Supermercado.Api.Models
+{
+    public class Venda
+    {
+        [Key]
+        public int VendaId { get; set; }
+
+        public ICollection<VendaProduto> VendaProdutos { get; set; } = new List<VendaProduto>();
+
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+    }
+}
---
+++ Ec-Supermercado.Api/Models/VendaProduto.cs
@@ -0,0 +1,13 @@
+﻿namespace Ec_Supermercado.Api.Models
+{
+    public class VendaProduto
+    {
+        public int VendaId { get; set; } 
+        public Venda Venda { get; set; }  
+
+        public int ProdutoId { get; set; }  
+        public Produto Produto { get; set; }  
+
+        public int Quantidade { get; set; }
+    }
+}
