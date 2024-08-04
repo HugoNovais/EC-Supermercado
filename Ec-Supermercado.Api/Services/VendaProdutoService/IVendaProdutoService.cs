9ba5444f53cce6ae185e160e0140347039db7a79
HugoNovais <hugo.o.novais@outlook.com>
2024-08-04 20:44:37 -0300

    VendaProduto Service

---
+++ Ec-Supermercado.Api/Services/VendaProdutoService/IVendaProdutoService.cs
@@ -0,0 +1,13 @@
+﻿using Ec_Supermercado.Api.DTOs;
+
+namespace Ec_Supermercado.Api.Services.VendaProdutoService
+{
+    public interface IVendaProdutoService
+    {
+        Task<IEnumerable<VendaProdutoDTO>> GetVendaProdutos();
+        Task<VendaProdutoDTO> GetVendaProduto(int id);
+        Task AddVendaProduto (VendaProdutoDTO vendaProduto);
+        Task UpdateVendaProduto(VendaProdutoDTO vendaProduto);
+        Task DeleteVendaProduto (int id);
+    }
+}
---
+++ Ec-Supermercado.Api/Services/VendaProdutoService/VendaProdutoService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Ec_Supermercado.Api.DTOs;
+using Ec_Supermercado.Api.Models;
+using Ec_Supermercado.Api.Repositories.VendaProdutoRepository;
+
+namespace Ec_Supermercado.Api.Services.VendaProdutoService
+{
+    public class VendaProdutoService : IVendaProdutoService
+    {
+
+        private readonly IVendaProdutoRepository _repository;
+        private readonly IMapper _mapper;
+
+        public VendaProdutoService(IVendaProdutoRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<VendaProdutoDTO>> GetVendaProdutos()
+        {
+            var vendaProdutoEntity = await _repository.GetAll();
+            return _mapper.Map<IEnumerable<VendaProdutoDTO>>(vendaProdutoEntity);
+        }
+
+        public async Task<VendaProdutoDTO> GetVendaProduto(int id)
+        {
+            var vendaProdutoEntity = await _repository.GetById(id);
+            return _mapper.Map<VendaProdutoDTO>(vendaProdutoEntity);
+        }
+
+        public async Task AddVendaProduto(VendaProdutoDTO vendaProduto)
+        {
+            var vendaProdutoEntity = _mapper.Map<VendaProduto>(vendaProduto);
+            await _repository.Create(vendaProdutoEntity);
+            vendaProduto.VendaProdutoId = vendaProdutoEntity.VendaProdutoId;
+        }
+
+
+        public async Task UpdateVendaProduto(VendaProdutoDTO vendaProduto)
+        {
+            var vendaProdutoEntity = _mapper.Map<VendaProduto>(vendaProduto);
+            await _repository.Update(vendaProdutoEntity);
+        }
+
+        public async Task DeleteVendaProduto(int id)
+        {
+            var vendaProdutoEntity =  _repository.GetById(id).Result;
+            await _repository.DeleteById(vendaProdutoEntity.VendaId);
+        }
+
+ 
+    }
+}
